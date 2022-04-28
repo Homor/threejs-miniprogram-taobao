f4687ae90d172659d49178367169a0fbde9422d5
molor <yeahmolo@163.com>
2022-04-28 18:14:28 +0800

    优化点击处理逻辑

--- README.md
+++ README.md
@@ -2,11 +2,12 @@
  * @Author: molor
  * @Date: 2022-04-26 14:30:10
  * @LastEditors: molor
- * @LastEditTime: 2022-04-28 16:51:50
+ * @LastEditTime: 2022-04-28 18:13:19
  * @FilePath: \threejs-miniprogram-taobao\README.md
 -->
 # 淘宝 商家应用，three.js的适配
 
 ## 更新日志
-1.0.0 初版  
-1.0.1 兼容点击事件中x或y参数不存在的问题
+1.0.0 初版;
+1.0.1 兼容点击事件中x或y参数不存在的问题;
+1.0.2 优化点击处理逻辑;
--- src/Touch.js
+++ src/Touch.js
@@ -2,7 +2,7 @@
  * @Author: molor
  * @Date: 2022-04-26 11:51:58
  * @LastEditors: molor
- * @LastEditTime: 2022-04-28 16:48:42
+ * @LastEditTime: 2022-04-28 18:12:13
  * @FilePath: \threejs-miniprogram-taobao\src\Touch.js
  */
 export default function Touch(EventBus) {
@@ -17,43 +17,41 @@ export default function Touch(EventBus) {
       if (!e.touches || e.touches.length == 0) {
         console.log('无效点击事件,', type);
       } else {
-        e.touches.forEach(item => {
-          if(item.x!==undefined){
+        if (e.touches[0].x !== undefined) {
+          e.touches.forEach(item => {
             item.pageX = item.x;
             item.pageY = item.y;
             item.clientX = item.x;
             item.clientY = item.y;
-          }
-        });
+          });
+        }
         EventBus.dispatchEvent(e);
       }
     } else if (type == 'touchEnd' || type == 'touchCancel') {
       if (!e.touches || e.touches.length == 0) {
         e.touches = [];
-      }else{
-        e.touches.forEach(item => {
-          if(item.x!==undefined){
+      } else {
+        if (e.touches[0].x !== undefined) {
+          e.touches.forEach(item => {
             item.pageX = item.x;
             item.pageY = item.y;
             item.clientX = item.x;
             item.clientY = item.y;
-          }
-
-        });
+          });
+        }
       }
 
       if (!e.changedTouches || e.changedTouches.length == 0) {
         console.log('无效点击事件,', type);
       } else {
-        e.changedTouches.forEach(item => {
-          if(item.x!==undefined){
+        if (e.changedTouches[0].x !== undefined) {
+          e.changedTouches.forEach(item => {
             item.pageX = item.x;
             item.pageY = item.y;
             item.clientX = item.x;
             item.clientY = item.y;
-          }
-   
-        });
+          });
+        }
         EventBus.dispatchEvent(e);
       }
     }
